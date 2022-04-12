8779e6510e5a6e9e2be277b04058665b9dae1bac
Adrian Edelen <adrianedelen@gmail.com>
2022-04-12 09:10:05 -0400

    package files, testing ref data, test files create

---
+++ test/test_cache.js
@@ -0,0 +1,35 @@
+//mocha test example for syntax
+// var assert = require('assert');
+// describe('Array', function () {
+//   describe('#indexOf()', function () {
+//     it('should return -1 when the value is not present', function () {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+// });
+
+
+
+
+
+function TestTimingData() {
+  var response = [];
+  var t1 = 12;
+  
+}
+function TestTimingAppData(){}
+function TestCarData() {}
+function TestPositionData() {}
+function TestTrackStatusData() {}
+function TestSessionStatusData() {}
+function TestWeatherData() {}
+
+
+TestTimingData();
+TestTimingAppData();
+TestCarData();
+TestPositionData();
+TestTrackStatusData();
+TestTrackStatusData();
+TestSessionStatusData();
+TestWeatherData();
